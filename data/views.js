d341b4f5f3a55f07b6e7d8e23c3e719808da2c1e
juko7575 <juko7575@gmail.com>
2026-06-28 19:19:43 +0900

    Add files via upload

---
+++ data/views.js
@@ -0,0 +1,39 @@
+window.VIEWS = {
+
+    antiTank: {
+
+        title: "Anti-Tank Documentation",
+
+        layout: "standard",
+
+        entities: [
+            "panzerfaust", "bazooka", "dragon", "jav"
+        ]
+
+    },
+
+    armor: {
+
+        title: "Armor Documentation",
+
+        layout: "standard",
+
+        entities: [
+
+        ]
+
+    },
+
+    configs: {
+
+        title: "Mod Config Files",
+
+        layout: "large",
+
+        entities: [
+            "modlist_default", "modlist_editor", "modlist_photos"
+        ]
+
+    }
+
+};
